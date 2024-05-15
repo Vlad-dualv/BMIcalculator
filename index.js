3a9344069c490262ce2dfb3193fca7d71d3048d5
Vlad <vladswork95@gmail.com>
2024-05-15 18:30:15 +0100

    Update

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ function calculateBMI() {
   bmiInputElement.value = bmiValue.toFixed(1);
 
   if (bmiValue < 18.5) {
-    weightConditionElement.innerText = 'Underweight';
+    weightConditionElement.innerText =
+      'Underweight \n (Healthy BMI: 18.5–24.9)';
   } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
-    weightConditionElement.innerText = 'Normal Weight';
+    weightConditionElement.innerText =
+      'Normal Weight \n (Healthy BMI: 18.5–24.9)';
   } else if (bmiValue >= 25 && bmiValue <= 29.9) {
-    weightConditionElement.innerText = 'Overweight';
+    weightConditionElement.innerText = 'Overweight \n (Healthy BMI: 18.5–24.9)';
   } else {
-    weightConditionElement.innerText = 'Obesity';
+    weightConditionElement.innerText = 'Obesity \n (Healthy BMI: 18.5–24.9)';
   }
 }
 
